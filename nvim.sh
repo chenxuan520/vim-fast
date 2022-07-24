d7dd7a152a3228730102ade6a8141908f77805e2
chenxuan <1607772321@qq.com>
2022-07-24 17:29:33 +0800

    nvim config

---
+++ nvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'it will change init.vim please make sure init.vim has be stored'
+read -p "continue?[y/n]" ch
+if [[ $ch == "n" ]] || [[ $ch == "N" ]]; then
+	return
+fi
+type nvim
+if [ $? == 1 ];then
+	sudo apt update
+	sudo apt install neovim
+fi
+ln -s ./.vimrc ~/.config/nvim/init.vim
+cp -r ./colors ~/.config/nvim/
