cb1513abf9f7836a645fd40a5a309cdf0be66ab3
chenxuan <1607772321@qq.com>
2022-07-24 17:50:58 +0800

    fix

--- nvim.sh
+++ nvim.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo 'it will change init.vim please make sure init.vim has be stored'
-read -p "continue?[y/n]" ch
-if [[ $ch == "n" ]] || [[ $ch == "N" ]]; then
-	return
-fi
-type nvim
-if [ $? == 1 ];then
-	sudo apt update
-	sudo apt install neovim
-fi
-rm ~/.config/nvim/init.vim
-mkdir ~/.config/nvim
-ln -s ./.vimrc ~/.config/nvim/init.vim 
-cp -r ./colors ~/.config/nvim/
+function main(){
+	echo 'it will change init.vim please make sure init.vim has be stored'
+	read -p "continue?[y/n]" ch
+	if [[ $ch == "n" ]] || [[ $ch == "N" ]]; then
+		return
+	fi
+	type nvim
+	if [ $? == 1 ];then
+		sudo apt update
+		sudo apt install neovim
+	fi
+	rm ~/.config/nvim/init.vim
+	mkdir ~/.config/nvim
+	ln -s $PWD/.vimrc ~/.config/nvim/init.vim 
+	cp -r ./colors ~/.config/nvim/
+	cp -r ./autoload ~/.config/nvim/
+	cp -r ./ftplugin ~/.config/nvim/
+}
+main
