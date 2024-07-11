930f188cb523643025505708e0c0373529ab50fb
chenxuan <1607772321@qq.com>
2024-07-12 00:49:59 +0800

    feat:test githubcicd

--- shell/plug.sh
+++ shell/plug.sh
@@ -2,5 +2,6 @@
 rm ./vim.tar.gz
 wget https://gitee.com/mirrorvim/vim-fast/releases/download/v1.0.14/vim.tar.gz
 tar -zxvf ${PWD}/vim.tar.gz -C ~/.vim/plugged/
+ls -alh ~/.vim/plugged
 
 git config --global --add safe.directory "*"
