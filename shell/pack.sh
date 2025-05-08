f355866ef3b81063f015cec7a1f52d8adb3e321e
chenxuan <1607772321@qq.com>
2025-05-09 02:11:25 +0800

    refactor: update vim-fast version and file operations

--- shell/pack.sh
+++ shell/pack.sh
@@ -2,11 +2,14 @@
 
 # this file is for author pack the plug,don't run it
 
+ignore="$HOME/.vim/plugged/coc.nvim $HOME/.vim/plugged/copilot.vim $HOME/.vim/plugged/tokyonight.nvim"
+temp="$HOME/.vim/coc.nvim $HOME/.vim/copilot.vim $HOME/.vim/tokyonight.nvim"
+
 rm ./vim.tar.gz
-mv ~/.vim/plugged/coc.nvim ~/.vim/
+mv $ignore ~/.vim/
 cd ~/.vim/plugged
 tar -zcvf ~/vim-fast/vim.tar.gz -C ~/.vim/plugged ./*
-mv ~/.vim/coc.nvim ~/.vim/plugged/
+mv $temp ~/.vim/plugged/
 
 mkdir -p ~/vim-fast/plugged
 tar -zxvf ~/vim-fast/vim.tar.gz -C ~/vim-fast/plugged/
--- shell/plug.sh
+++ shell/plug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm ./vim.tar.gz
-wget https://gitee.com/mirrorvim/vim-fast/releases/download/v1.0.15/vim.tar.gz
+wget https://gitee.com/mirrorvim/vim-fast/releases/download/v1.0.16/vim.tar.gz
 tar -zxvf ${PWD}/vim.tar.gz -C ~/.vim/plugged/
 ls -alh ~/.vim/plugged
 
